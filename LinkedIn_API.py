225ffc5e97cfd4aadb5c513cd5526be47628de84
John Doe <doe123@gmail.com>
2017-11-12 23:27:18 -0800

    created user class for LinkedIn users

--- LinkedIn_API.py
+++ LinkedIn_API.py
@@ -7,27 +7,27 @@ from linkedin import linkedin
 
 class linkedinUser(object):
 
-    def __init__(self, key, secret, redirect_uri, permissions=None):
-        self.key = key
-        self.secret = secret
-        self.redirect_uri = redirect_uri
-        self.permissions = permissions or []
-        self.state = None
-        self.authorization_code = None
-        self.token = None
-        self._error = None
-
+    def __init__(self, firstName, headline, lastName):
+        """
+        Return LinkinIn user object with a first name, last name, and job title
+        """
+        self.firstName = firstName
+        self.headline = headline
+        self.lastName
+'''
 def searchLinkedIn(user):
-    CONSUMER_KEY = '86rgu0o94324wm'
-    CONSUMER_SECRET = 'Iixm993VasP2WgYf'
-    USER_TOKEN = '4302aea7-cd3d-41e4-b511-48041a582e96'
-    USER_SECRET = 'ea4f089f-d2ea-4feb-b750-96811c4c4e66'
-    RETURN_URL = 'http://localhost:8000'
-    authentication = linkedin.LinkedInDeveloperAuthentication(CONSUMER_KEY, CONSUMER_SECRET, USER_TOKEN, USER_SECRET, RETURN_URL, linkedin.PERMISSIONS.enums.values())
+
+    authentication = linkedin.LinkedInDeveloperAuthentication('86rgu0o94324wm', 'Iixm993VasP2WgYf', '4302aea7-cd3d-41e4-b511-48041a582e96', 'ea4f089f-d2ea-4feb-b750-96811c4c4e66', 'http://localhost:8000', linkedin.PERMISSIONS.enums.values())
     application = linkedin.LinkedInApplication(authentication)
+
     results = application.get_profile()
     {u'firstName': u'Dylan',
     u'headline': u'',
     u'lastName': u'Davis',
     u'siteStandardProfileRequest': {u'url': u'http://www.linkedin.com/profile/view?id=46113651&authType=name&authToken=E4302aea7-cd3d-41e4-b511-48041a582e96'}}
     print(results)
+    #users = []
+
+    #for user in results:
+     #   users.append(LinkedInUser()
+'''
