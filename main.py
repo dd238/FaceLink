c53209a21c6ace436b1d5f13988233905c3af2df
Samy Achour <sachour96@gmail.com>
2017-11-12 16:31:45 -0800

    small changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class twitterUser(object):
 
 	def __init__(self, name, username, location):
 		"""
-        Return a User object whose full name is *name*,
+        Return a twitter user object whose full name is *name*,
         twitter username is *username*,
         location is *location*
         """
@@ -14,15 +14,15 @@ class twitterUser(object):
 
 def searchTwitter(user):
 	"""
-    Searches twitter for user, returns first 5 results
+    Searches twitter for user, returns results
     ----------
     user : string
-        1-dimensional array with input signal data
+        user name query
 
     Returns
     -------
-    data : array_like
-        1-dimensional array with cleaned up signal data
+    users : array
+        1-dimensional array of search results - twitter user objects
     """
 	twitter = Twitter(
 			auth = OAuth("754771372996435968-uJ7WYrDBgd0WslQv1nzYb62xmOfW2dr", "ARxSD4Wa7q8ih41aF13cvsmw1hOmjz1B8jdihhTBWdotX", "5C6fklOsuiPNBqUjcFBm2lw5Q", "91gLoRw2MOEmxInmlosjyUVxdgXeWDG8fn0bhcXab7tCt6aOab"))
