092a36bc39b60ff99c3da3b9dc3350c1b8c61276
Samy Achour <sachour96@gmail.com>
2017-11-28 16:13:51 -0800

    added some documentation

--- main/socialLink/models.py
+++ main/socialLink/models.py
@@ -5,6 +5,22 @@ from django.db import models
 
 class Tweet:
     def __init__(self, body="", date=""):
+        """
+        Return a tweet object whose text is *body*,
+        creation date is *date*,
+        objectionable rating is *rating*
+        """
         self.body = body
         self.date = date
         self.rating = 0
+
+class TwitterUser:
+    def __init__(self, name, username, location):
+        """
+        Return a twitter user object whose full name is *name*,
+        twitter username is *username*,
+        location is *location*
+        """
+        self.name = name
+        self.username = username
+        self.location = location
